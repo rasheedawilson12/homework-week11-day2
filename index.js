757a92f9017f574b0c9d57583c99b5f2639e14c3
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-08-08 18:27:27 -0500

    Greetings express application created

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
+const names = ["Greg", "Peter", "Bobby", "Marcia", "Jan", "Cindy"];
+
 // greeting
 app.get("/greeting", (req, res) => {
   res.send("<h1>Hello, stranger</h1>");
+});
+
+app.get("/:indexOfNamesArray", (req, res) => {
+  res.send("Hello, " + names[req.params.indexOfNamesArray]);
 });
 
 app.listen("3000", (req, res) => {
